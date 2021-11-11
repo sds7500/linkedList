2b266a6a24a25d5bf729a325d52daf33c002238e
sds7500 <somyasheti@gmail.com>
2021-11-11 16:16:21 +0530

    insertInSortedOrder

---
+++ insert_in_sorted_linked_list.java
@@ -0,0 +1,16 @@
+public class insert_in_sorted_linked_list {
+    Node sortedInsert(Node head, int data) {
+        Node node=new Node(data);
+        if(head==null)return node;
+        if(head.data>data){
+            node.next=head;
+            return node;
+        }
+        Node curr=head;
+        while(curr.next!=null && curr.next.data<data)
+            curr=curr.next;
+        node.next=curr.next;
+        curr.next=node;
+        return head;
+    }
+}
